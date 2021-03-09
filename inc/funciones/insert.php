61f7fa46fdd7552e2d4f5fcb8f2d529887644181
Sofia Bracho <sofia_bracho1@hotmail.com>
2021-03-09 08:17:54 -0400

    New alerts on buttons & modified login form
    Fixed misspath on insert.php

--- inc/funciones/insert.php
+++ inc/funciones/insert.php
@@ -1,5 +1,5 @@
 <?php
-    require_once "../inc/funciones/db.php";
+    require_once "db.php";
     session_start();
 
     foreach ($_POST as $key => $data) {
--- js/upload-data.js
+++ js/upload-data.js
@@ -25,9 +25,19 @@ $(function(){
                     let resultado = data;
                    console.log(resultado)
                     if(resultado.respuesta == 'exito') {
-                        console.log("Se han insertado todas las entradas en la base de datos!");
+                        swal({
+                            type: "success",
+                            title: "Importado",
+                            text: "Se han insertado todas las entradas en la base de datos!",
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
                     } else {
-                        console.log("Ha habido un error al insertar las entradas en la base de datos");                   
+                        swal({
+                            type: "error",
+                            title: "Error",
+                            text: "Ha habido un error al insertar las entradas en la base de datos"
+                        })
                     }
                 }
             });
